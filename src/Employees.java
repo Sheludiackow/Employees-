73d28227279f9fca10bbc00ef2c871693eca2f0a
Sheludiackow <leshiy.ru@bk.ru>
2017-01-30 09:29:52 +0500

    Закончена работа с выводом сотрудников. Сделана работа со *

--- src/Employees.java
+++ src/Employees.java
@@ -3,53 +3,24 @@
  * с полями: ФИО, должность, email, телефон, зарплата, возраст;
  */
 public class Employees {
-    String surname;    // Фамилия
-    String name;       // Имя
-    String patronymic; // Отчество
-    String position;   // Должность
-    String email;      // Почта
-    String phone;      // Телефон
-    int age;           // Возраст
-    int salary;        // Зарплата
+    private String fio;
+    private int age;
+    private String email;
 
-    public Employees(String surname, String name, String patronymic, String position, String email, String phone, int age, int salary){
-        this.surname = surname;
-        this.name = name;
-        this.patronymic = patronymic;
-        this.position = position;
+    // Нажимаем Alt + insert и создаем конструктор
+
+
+    public int getAge() {  //Создали "геттер" для "age"
+        return age;
+    }
+
+    public Employees(String fio, String email, int age) {
+        this.fio = fio;
         this.email = email;
-        this.phone = phone;
         this.age = age;
-        this.salary = salary;
     }
-
-    public static void genEmployes (Employee[] employes) {
-        for(int i = 0; i < employes.length; i++) {
-            employes[i] = EmployeeGenerator.getEmployee();
-        }
-    }
-    private static void toDisplayQuery(Employee[] employes, String where, String what, String less_than, int condition ) {
-        if ("age".equals(what)) {
-            for (int i = 0; i < employes.length; i++) {
-                if(employes[i].age > 40) {
-                    System.out.println(employes[i]);
-                    System.out.println();// Отступ для красоты
-                }
-            }
-        }
-    }
-
 
     public void printInfo(){
-        System.out.println("Фамилия: "+surname);
-        System.out.println("Имя: "+name);
-        System.out.println("Отчество: "+patronymic);
-        System.out.println("Должность: "+position);
-        System.out.println("Почта: "+email);
-        System.out.println("Телефон: "+phone);
-        System.out.println("Возраст: "+age);
-        System.out.println("Зарплата: "+salary);
+        System.out.println("fio "+fio+"email"+email+"age "+age);
     }
-
-
 }
--- src/MainClass.java
+++ src/MainClass.java
@@ -1,19 +1,27 @@
-import java.util.Scanner;
 
 /**
- * Created by Sheludiackow on 26.01.2017.
+ * 1)Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст;
+ * 2)Конструктор класса должен заполнять эти поля при создании объекта;
+ * 3)Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль;
+ * 4)Создать массив из 5 сотрудников. С помощью цикла вывести информацию только о сотрудниках старше 40 лет; Пример: ```
+ * Person [] persArray = new Person [ 5 ]; // Сначала объявляем массив объектов,
+ * persArray [ 0 ] = new Person ( "Ivanov Ivan" , "Engineer" , " ivivan@mailbox.com " , "892312312" , 30000 ,30 ); // потом для каждой ячейки массива задаем объект
+ * persArray [ 1 ] = new Person (...);
+ * ...
+ * persArray [ 4 ] = new Person (...);
  */
 public class MainClass {
     public static void main(String[] args) {
-        Employee[] employes = new Employee[5];
-        fillEmployes(employes);
-        Scanner sc = new Scanner(System.in);
-        System.out.println("1 - Вывести список сотрудников");
-        System.out.println("2 - Добавить сотрудника");
-        System.out.println("3 - Поиск сотрудников старше 40 лет");
-    }
-
-    public static void person() {
-
+        Employees[] employees = new Employees[5];
+        employees[0] = new Employees("Ivanov","ivanov@mail.ru",45);
+        employees[1] = new Employees("Ivanov1","ivanov@mail.ru",36);
+        employees[2] = new Employees("Ivanov2","ivanov@mail.ru",52);
+        employees[3] = new Employees("Ivanov3","ivanov@mail.ru",27);
+        employees[4] = new Employees("Ivanov4","ivanov@mail.ru",48);
+        for (int i = 0; i < employees.length; i++) {
+            if(employees[i].getAge() > 40){
+                employees[i].printInfo();
+            }
+        }
     }
 }
